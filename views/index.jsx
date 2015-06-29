5e1ce9d51b3a0720a7a0bec945e60a354678b4af
gb714us <gb714us@yahoo.com>
2015-06-29 16:35:51 -0700

    css complete

--- views/index.jsx
+++ views/index.jsx
@@ -1,57 +1,69 @@
-    var React = require('react');
+var React = require('react');
 
-    var TodoBox = React.createClass({
-      // Omitted
-      render: function() {
-        return (
-          <div className="todoBox">
-            <h1>Todos</h1>
-            <TodoList />
-            <TodoForm />
-          </div>
-          );
-      }
-    });
+var style = {
+  border: "1px solid black"
+};
 
-    var TodoList = React.createClass({
-      render: function() {
-        return (
-          <div className = "todoList">
-            <table style={{border: "2px solid black"}}>
-              <tbody>
-                <Todo title="Shopping">Milk</Todo>
-                <Todo title="Hair cut">13:00</Todo>
-              </tbody>
-            </table>
-          </div>
-        );
-      }
-    });
 
-    var Todo = React.createClass({
-      // Write code here
-      protoTypes: {
-        title: React.PropTypes.number.isRequired
-      },
-      render: function(){
-        return (
-          <tr>
-            <td style={{border: "1px solid black"}}>{this.props.title}</td>
-            <td style={{border: "1px solid black"}}>{this.props.children}</td>
-          </tr>
-          );
-      }
-    });
+var TodoBox = React.createClass({
+  // Omitted
+  render: function() {
+    return (
+      <div className="todoBox">
+        <h1>Todos</h1>
+        <TodoList />
+        <TodoForm />
+      </div>
+    );
+  }
+});
 
-    var TodoForm = React.createClass({
-      // Omitted
-      render: function(){
-        return (
-          <div className="todoForm">
-            I am a TodoForm.
-          </div>
-          );
-      }
-    });
+var TodoList = React.createClass({
+  render: function() {
+    return (
+      <div className = "todoList">
+        <table style={{border: "2px solid black"}}>
+          <tbody>
+            <Todo title="Shopping">Milk</Todo>
+            <Todo title="Hair cut">13:00</Todo>
+          </tbody>
+        </table>
+      </div>
+    );
+  }
+});
 
-    module.exports = TodoBox;
+var Todo = React.createClass({
+  getInitialState: function(){
+    return {checked: false};
+  },
+  // Write code here
+  handleChange: function(e){
+    this.setState({checked: e.target.checked});
+  },
+  protoTypes: {
+    title: React.PropTypes.number.isRequired
+  },
+  render: function(){
+    return (
+      <tr>
+        <td style={style}><input type="checkbox" checked={this.state.checked} onChange={this.handleChange} /></td>
+        <td style={style}>{this.props.title}</td>
+        <td style={style}>{this.props.children}</td>
+      </tr>
+    );
+  }
+});
+
+var TodoForm = React.createClass({
+  // Omitted
+  render: function(){
+    return (
+      <div className="todoForm">
+        I am a TodoForm.
+      </div>
+    );
+  }
+});
+
+module.exports = TodoBox;
